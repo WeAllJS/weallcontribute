5197c71a3ca114f2ab134f0255fc581351f91862
Kat Marchán <kzm@sykosomatic.org>
2017-02-19 11:10:15 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# weallcontribute [![npm version](https://img.shields.io/npm/v/weallcontribute.svg)](https://npm.im/weallcontribute) [![license](https://img.shields.io/npm/l/weallcontribute.svg)](https://npm.im/weallcontribute) [![Travis](https://img.shields.io/travis/wealljs/weallcontribute.svg)](https://travis-ci.org/wealljs/weallcontribute) [![AppVeyor](https://ci.appveyor.com/api/projects/status/github/wealljs/weallcontribute?svg=true)](https://ci.appveyor.com/project/wealljs/weallcontribute) [![Coverage Status](https://coveralls.io/repos/github/wealljs/weallcontribute/badge.svg?branch=latest)](https://coveralls.io/github/wealljs/weallcontribute?branch=latest)
+
+[`weallcontribute`](https://npm.im/weallcontribute) is a command-line tool for automatically generating and updating [`CONTRIBUTING.md`](https://github.com/blog/1184-contributing-guidelines) guidelines for your projects.
+
+## Install
+
+#### Locally to your npm project (recommended):
+
+`$ npm install --save-dev weallcontribute`
+
+#### Globally:
+
+`$ npm install -g weallcontribute`
+
+## Example
+
+### npm repo
+```javascript
+// package.json
+{
+  "scripts": {
+    "update-contrib": "weallcontribute -o . && git commit add CONTRIBUTING.md && git commit -m 'docs(contributing): updated CONTRIBUTING.md'"
+  }
+}
+// Now you can do `npm run update-contrib` any time you
+// bump your `weallcontribute` version to bring your docs
+// up to date! And you don't need a global install!
+```
+
+### Global CLI install
+```sh
+# Read your repo info from package.json or git
+# and pipe the output to `contribs.md`
+$ weallcontribute > contribs.md
+
+# Write a CONTRIBUTING.md document for org/proj to the ./foo dir.
+$ weallcontribute org/proj -o ./foo
+```
---
+++ index.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var hostedGitInfo = require('hosted-git-info')
+var path = require('path')
+
+var yargs = require('yargs')
+.usage('Usage: $0 [repository] [options]')
+.example('$0 wealljs/weallcontribute > contrib.md', 'Output a contrib doc to stdout and pipe to contrib.md')
+.example('$0 -o .', 'Read project repo from package.json or git config, then write `CONTRIBUTING.md` to current directory (.).')
+.alias('o', 'output')
+.nargs('o', 1)
+.normalize('o')
+.describe('o', 'Output to directory or make new file.')
+.help()
+
+var repo = yargs.argv._[0]
+if (!repo) {
+  try {
+    repo = require('./package.json').repository
+    repo = repo.url || repo
+    console.warn('Using project repo from package.json')
+  } catch (e) { console.error(e) }
+}
+if (!repo) {
+  try {
+    repo = require('child_process').spawnSync('git', ['remote', '-v'], {encoding: 'utf8'}).output[1].match(/origin\s+([^\s]+)\s/)[1]
+    console.warn('Using project repo from current git repo')
+  } catch (e) {}
+}
+if (!repo) {
+  console.error('Unable to figure out your repo.\n')
+  yargs.showHelp()
+  process.exit(1)
+}
+
+repo = hostedGitInfo.fromUrl(repo)
+if (!repo) {
+  console.error('Invalid repository url or shorthand.\n')
+  yargs.showHelp()
+  process.exit(1)
+}
+if (repo.type !== 'github') {
+  console.error('Only github repositories are supported (for now).\n')
+  yargs.showHelp()
+  process.exit(1)
+}
+
+var contrib = fs.readFileSync(
+  path.join(__dirname, 'CONTRIBUTING.md'),
+  'utf8'
+).replace(/wealljs\/weallcontribute/g, repo.path())
+
+if (!yargs.argv.output) {
+  console.log(contrib)
+  process.exit(0)
+}
+
+var target = yargs.argv.output
+try {
+  if (fs.statSync(target).isDirectory()) {
+    target = path.join(target, 'CONTRIBUTING.md')
+  }
+} catch (e) {}
+
+fs.writeFileSync(target, contrib)
+console.log('Contrib docs written to', target)
+process.exit(0)
